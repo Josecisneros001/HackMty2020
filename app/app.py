f216bacc168e915f3fbf7ae81770d37c3458312c
zJuarez <mji2000.741@gmail.com>
2020-08-30 01:19:09 -0500

    BOT JOINING CORRECTLY

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from colors import *
+from bot import *
 
 root = Tk()
 root.title('Zoomer')
@@ -7,45 +8,53 @@ root.iconbitmap()
 root.geometry("230x1000")
 root.config(bg=black)
 
-id = "123983213"
-
 def getParticipants():
-      pass
+    pass
 
 def getHands():
-      pass
+    pass
 
 def exit():
-      start_frame.pack_forget()
-      
-      #start_frame 
+    start_frame.pack_forget()
+
 def go():
-      start_frame.pack(fill="both", expand=1)
-      title1=Label(start_frame, text='Zoomer', fg=blue, bg= black, font=('Lato', 23, 'bold'))
-      title1.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-      id = root.clipboard_get()
-      info = Label(start_frame, height=2, text="ID:\n" + id, fg=grey, bg= black, font=('Lato', 12))
-      info.place(relx=0.5, rely=0.2, anchor=CENTER)
+    clipboard = root.clipboard_get()
+    mainBot(clipboard)
 
-      button = Button(start_frame, text='Get Participants', command=getParticipants, font=('Lato', 12), bg = blue, fg=platinum)     
-      button.place(relx=0.5, rely=0.4, anchor=CENTER)
+    start_frame.pack(fill="both", expand=1)
+    title1 = Label(start_frame, text='Zoomer', fg=blue,
+                   bg=black, font=('Lato', 23, 'bold'))
+    title1.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-      button = Button(start_frame, text='Count Hands', command=getHands, font=('Lato', 12), bg = blue, fg=platinum)     
-      button.place(relx=0.5, rely=0.5, anchor=CENTER)
+    info = Label(start_frame, height=2, text="ID:\n" +
+                 id, fg=grey, bg=black, font=('Lato', 12))
+    info.place(relx=0.5, rely=0.2, anchor=CENTER)
 
-      button = Button(start_frame, text='Exit', command=exit, font=('Lato', 12), bg = orange, fg=platinum)     
-      button.place(relx=0.5, rely=0.83, anchor=CENTER)
+    button = Button(start_frame, text='Get Participants',
+                    command=getParticipants, font=('Lato', 12), bg=blue, fg=platinum)
+    button.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-#main
-title=Label( text='Zoomer', fg=blue, bg= black, font=('Lato', 23, 'bold'))
+    button = Button(start_frame, text='Count Hands',
+                    command=getHands, font=('Lato', 12), bg=blue, fg=platinum)
+    button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    button = Button(start_frame, text='Exit', command=exit,
+                    font=('Lato', 12), bg=orange, fg=platinum)
+    button.place(relx=0.5, rely=0.83, anchor=CENTER)
+
+
+# main
+title = Label(text='Zoomer', fg=blue, bg=black, font=('Lato', 23, 'bold'))
 title.place(relx=0.5, rely=0.1, anchor=CENTER)
 
 
-info = Label( height=3, text='Copy the meeting-id \n in your clipboard \n and hit go!', fg=grey, bg= black, font=('Lato', 12))
+info = Label(height=3, text='Copy the meeting link \n in your clipboard \n and hit go!',
+             fg=grey, bg=black, font=('Lato', 12))
 info.place(relx=0.5, rely=0.3, anchor=CENTER)
 
-button = Button(text='Go!', command=go, font=('Lato', 16), bg = blue, fg=platinum)     
+button = Button(text='Go!', command=go, font=(
+    'Lato', 16), bg=blue, fg=platinum)
 button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 # create start frame
